5c6b311577200ff6b54162fb423e87ed80e3c0ff
FayeZ <im5dii@gmail.com>
2020-06-09 19:04:29 -0700

    First Commit

---
+++ app/src/main/java/com/laioffer/eventreporter/EventsFragment.java
@@ -0,0 +1,73 @@
+package com.laioffer.eventreporter;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventsFragment extends Fragment {
+    private ImageView mImageViewAdd;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private DatabaseReference database;
+    private List<Event> events;
+
+    public EventsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        mImageViewAdd = (ImageView) view.findViewById(R.id.img_event_add);
+
+        mImageViewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getActivity(), EventReportActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        return view;
+    }
+//
+//    public void setAdapter() {
+//        events = new ArrayList<Event>();
+//        database.child("events").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+//                    Event event = noteDataSnapshot.getValue(Event.class);
+//                    events.add(event);
+//                }
+//                mAdapter = new EventListAdapter(events);
+//                recyclerView.setAdapter(mAdapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                //TODO: do something
+//            }
+//        });
+//    }
+
+}
